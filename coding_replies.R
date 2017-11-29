f3b60df5a7dc4b5045a55a20fc5977ad596b2e5b
Charles Crabtree <ccrabtr@umich.edu>
2017-11-28 21:43:01 -0500

    Add files via upload

--- coding_replies.R
+++ coding_replies.R
@@ -11,35 +11,26 @@ library(tm)
 library(tm.plugin.mail)
 library(parallel)
 
-### Set working directory
-setwd("~/Dropbox/butler-crabtree/awareness/replication_files/collecting_outcomes_example/")
+### Convert exported .mbox file into individual emails that are saved in the specified directory.
+convert_mbox_eml("", dir = "")
 
-### Convert .mbox file into individual emails that are saved in the specified directory.
-# There are two emails in the .mbox file. One is from jsmith00508@outlook.com. The other is from crabtreec@yahoo.com.
-convert_mbox_eml("example.mbox", dir = "temp")
-
-### Load and inspect sample data
-# There are three people in our sample. The .mbox file contains emails from two of them. This means that after running all this code, we should have a response outcome measure that is 1 for two participants and 0 for 1 participant.  
-load("example-data.RData")
-head(data)
+### Load data
+load("")
 
 ### Change working directory to the directory where the individual emails were saved
-setwd("temp")
+setwd("")
 
 ### Create a corpus from the individual emails
 emails <- VCorpus(DirSource())
 
 ### Create outcome measure
-data$response <- 0
+response <- 0
 
 ### Code email responses
 i <- 1
 for(i in 1:length(emails))	{
   email_holder <- tolower(paste(unlist(emails[[i]]), collapse=""))
-  identified <- data$email[which(sapply(data$email, grepl, email_holder, fixed=TRUE))]
+  identified <- email[which(sapply(email, grepl, email_holder, fixed=TRUE))]
   if(length(identified)==0)	{next()}
-  data$response[i] <- 1 
+  response[i] <- 1 
 }
-
-### View data
-data
